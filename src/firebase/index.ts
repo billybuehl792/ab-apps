607d50758a8e7db92b41f845fb94680ade0b743b
Billy Buehl <billybuehl792@gmail.com>
2025-04-05 14:44:04 -0400

    update emulators

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,11 +2,23 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, connectAuthEmulator } from "firebase/auth";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
-import firebaseConfig from "../../firebase.config";
 import { getStorage } from "firebase/storage";
 import { initializeAppCheck, ReCaptchaV3Provider } from "@firebase/app-check";
 
-const app = initializeApp(firebaseConfig);
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const app = initializeApp({
+  apiKey: import.meta.env.VITE_PUBLIC_FIREBASE_API_KEY,
+  appId: import.meta.env.VITE_PUBLIC_FIREBASE_APP_ID,
+  authDomain: import.meta.env.VITE_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: import.meta.env.VITE_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: import.meta.env.VITE_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: import.meta.env.VITE_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_PUBLIC_FIREBASE_STORAGE_BUCKET,
+});
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
@@ -15,8 +27,8 @@ const storage = getStorage(app);
 if (import.meta.env.DEV) {
   self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
 
-  connectFirestoreEmulator(db, "127.0.0.1", 8080);
-  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+  connectAuthEmulator(auth, `http://127.0.0.1:${9099}`);
+  connectFirestoreEmulator(db, "127.0.0.1", 8081);
 }
 
 initializeAppCheck(app, {
